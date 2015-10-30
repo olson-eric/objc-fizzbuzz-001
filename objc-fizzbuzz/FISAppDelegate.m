b440c4f4ade59603ea753ff2117d60c55e81b55d
olson-eric <eao4dj@virginia.edu>
2015-10-29 21:17:52 -0400

    [eo] fizzy loops

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -7,11 +7,34 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /*
-     
-     * Write your code here!
-     
-     */
+    NSMutableString *fizzBuzz = [[NSMutableString alloc] init];
+    NSUInteger start = 1;
+    NSUInteger limit = 100;
+    
+    for (NSInteger i = start; i <= limit; i++) {
+        NSString *result = @"";
+        
+        if ((i % 5 == 0) && (i % 3 == 0)) {
+            result = @"FizzBuzz, ";
+        }
+        else if ((i % 5 == 0)) {
+            if (i == 100) {
+                result = @"Buzz";
+            }
+            else {
+                result = @"Buzz, ";
+            }
+        }
+        else if ((i % 3 == 0)) {
+            result = @"Fizz, ";
+        }
+        else {
+            result = [NSString stringWithFormat:@"%lu, ", i];
+        }
+        [fizzBuzz appendString:result];
+    }
+    
+    NSLog(@"%@", fizzBuzz);
     
     // do not alter
     return YES;  //
